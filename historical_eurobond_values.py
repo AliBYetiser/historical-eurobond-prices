741cd832591718181e3e5020d88109d720a4b5d3
Ali Berkay Yetiser <byetiser@hotmail.com>
2023-08-08 08:50:42 +0000

    Add Data

--- historical_eurobond_values.py
+++ historical_eurobond_values.py
@@ -18,10 +18,16 @@ file_path = dir_path.joinpath(file_name)
 
 service = Service(executable_path=r'/usr/bin/chromedriver')
 options = webdriver.ChromeOptions()
+user_agent = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36'    
+options.add_argument('user-agent={0}'.format(user_agent))
 options.add_argument('--headless')
 options.add_argument('--no-sandbox')
+options.add_argument('disable-infobars')
+options.add_argument('--disable-extensions')
+options.add_argument('--disable-gpu')
 options.add_argument('--disable-dev-shm-usage')
-driver = webdriver.Chrome(service=service, options=options)
+
+driver = webdriver.Chrome(service = service, options=options)
 
 # Get the website
 driver.get(
