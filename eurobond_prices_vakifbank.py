e4737f3b944a92ccf850e9bc534cf96386ac9fef
Ali Berkay Yetiser <byetiser@hotmail.com>
2023-08-11 12:51:19 +0000

    Update driver for page updates

--- eurobond_prices_vakifbank.py
+++ eurobond_prices_vakifbank.py
@@ -8,6 +8,13 @@ import time
 from pathlib import Path
 from datetime import datetime
 from selenium.webdriver.chrome.service import Service
+import os
+import sys
+from pyvirtualdisplay import Display
+
+
+display = Display(visible=0, size=(1024, 768))
+display.start()
 
 
 def append_data(rows, cols):
@@ -30,37 +37,20 @@ def find_item(i, j):
     ).text.replace(",", ".")
 
 
-def find_num_of_cols():
-    return len(
-        driver.find_elements(
-            By.XPATH,
-            "/html/body/form/div[5]/div[3]/div/div[1]/div/div[1]/div/div[2]/table/tbody/tr[1]/td",
-        )
-    )
-
-
-def find_num_of_rows():
-    return len(
-        driver.find_elements(
-            By.XPATH,
-            "/html/body/form/div[5]/div[3]/div/div[1]/div/div[1]/div/div[2]/table/tbody/tr",
-        )
-    )
-
-
 # define target file to write into
-dir_path = Path("./data/vakifbank/")
+script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+dir_path = Path(script_directory).joinpath(Path("./data/vakifbank/"))
 date = datetime.now().strftime("%d-%m-%Y")
 file_name_buy = "vakifbank(buy)_eurobond_" + date + ".csv"
 file_name_sell = "vakifbank(sell)_eurobond_" + date + ".csv"
 file_path_buy = dir_path.joinpath(file_name_buy)
 file_path_sell = dir_path.joinpath(file_name_sell)
 
-service = Service(executable_path=r"/usr/bin/chromedriver")
+service = Service() # Service(executable_path=r"/usr/bin/chromedriver")
 options = webdriver.ChromeOptions()
-user_agent = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36"
+user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0.0.0 Safari/537.36"
 options.add_argument("user-agent={0}".format(user_agent))
-options.add_argument("--headless")
+# options.add_argument("--headless")
 options.add_argument("--no-sandbox")
 options.add_argument("disable-infobars")
 options.add_argument("--disable-extensions")
@@ -68,27 +58,29 @@ options.add_argument("--disable-gpu")
 options.add_argument("--disable-dev-shm-usage")
 driver = webdriver.Chrome(service=service, options=options)
 # Get the website
+driver.implicitly_wait(10)
 driver.get(
     "https://www.vakifbank.com.tr/tahvil-bono-eurobond-hesaplama-araci.aspx?pageID=2378"
 )
-time.sleep(3)
-select_name = Select(driver.find_element("name", "ctl00$ctl10$ctl00$ddlTahvilBonoUrun"))
-time.sleep(3)
-select_name.select_by_value("2")
-time.sleep(3)
-select_action = Select(
-    driver.find_element("name", "ctl00$ctl10$ctl00$ddlTahvilIslemTipi")
-)
-select_action.select_by_value("1")
-time.sleep(3)
+driver.implicitly_wait(10)
+select = Select(driver.find_element(By.NAME, "ctl00$ctl10$ctl00$ddlTahvilBonoUrun"))
+select.select_by_value('2')
+driver.implicitly_wait(10)
+select_action = Select(driver.find_element(By.NAME, "ctl00$ctl10$ctl00$ddlTahvilIslemTipi"))
+select_action.select_by_value('1')
+driver.implicitly_wait(10)
 
+cols = len(driver.find_elements(By.XPATH,"/html/body/form/div[5]/div[3]/div/div[1]/div/div[1]/div/div[2]/table/tbody/tr[1]/td"))+1
+rows = len(driver.find_elements(By.XPATH,"/html/body/form/div[5]/div[3]/div/div[1]/div/div[1]/div/div[2]/table/tbody/tr"))+1
 header_row = []
-for p in range(2, find_num_of_cols() + 1):
+for p in range(2, cols):
     header_row.append(find_item(1, p))
 
 data = []
-append_data(find_num_of_rows() + 1, find_num_of_cols() + 1)
-
+print("num of cols: " + str(cols))
+print("num of rows: " + str(rows))
+append_data(rows, cols)
+time.sleep(1)
 # Write into the csv file
 with open(file_path_buy, "w", newline="") as csv_file:
     csv_writer = csv.writer(csv_file)
@@ -96,17 +88,16 @@ with open(file_path_buy, "w", newline="") as csv_file:
     for row in data:
         csv_writer.writerow(row)
 
-time.sleep(1)
-select_action.select_by_value("2")
-time.sleep(3)
-
-header_row = []
-for p in range(2, find_num_of_cols() + 1):
-    header_row.append(find_item(1, p))
-
+driver.implicitly_wait(10)
+select_name = Select(driver.find_element(By.NAME, "ctl00$ctl10$ctl00$ddlTahvilIslemTipi"))
+select_name.select_by_value('2')
+driver.implicitly_wait(10)
 data = []
-append_data(find_num_of_rows() + 1, find_num_of_cols() + 1)
+rows = len(driver.find_elements(By.XPATH,"/html/body/form/div[5]/div[3]/div/div[1]/div/div[1]/div/div[2]/table/tbody/tr"))+1
+print("sell rows: " +str(rows))
 
+append_data(rows, cols)
+time.sleep(1)
 
 # Write into the csv file
 with open(file_path_sell, "w", newline="") as csv_file:
@@ -114,3 +105,4 @@ with open(file_path_sell, "w", newline="") as csv_file:
     csv_writer.writerow(header_row)
     for row in data:
         csv_writer.writerow(row)
+
